c3e40129dff9eda54e885e355050b812baf547a7
Joe Versoza <jversoza@citytech.cuny.edu>
2012-02-01 02:35:22 -0500

    added example output to fizz_buzz

--- class1/hw/fizz_buzz.py
+++ class1/hw/fizz_buzz.py
@@ -3,6 +3,28 @@ FizzBuzz (via http://imranontech.com/2007/01/24/using-fizzbuzz-to-find-developer
 =====
 
 Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+
+Example Output:
+joe@walsh~/projects/archived/fizzbuzz$ python fizzbuzz.py
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+.
+.
+.
+
 """
 
 
