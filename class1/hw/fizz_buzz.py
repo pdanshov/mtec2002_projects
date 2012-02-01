80deb77825b2f1562ca2a3be1675c9e83d96890b
Joe Versoza <jversoza@citytech.cuny.edu>
2012-02-01 01:21:43 -0500

    added hw problem text as comments in source files

---
+++ class1/hw/fizz_buzz.py
@@ -0,0 +1,8 @@
+"""
+FizzBuzz (via http://imranontech.com/2007/01/24/using-fizzbuzz-to-find-developers-who-grok-coding/)
+=====
+
+Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+"""
+
+
---
+++ class1/hw/guess_number.py
@@ -0,0 +1,19 @@
+"""
+guess_number
+=====
+
+Write a program that stores a secret number in a variable.  When the program is run, it asks the user to enter a number.  If the number entered is the same as the secret number, print out "You got it!".  If the number entered is not equal to the secret number, print out whether or not it was too low or to high.
+
+Example ouptput:
+
+walsh:~ joe$ python /tmp/guess_number.py
+Guess what number I'm thinking of...  25
+Too high!
+walsh:~ joe$ python /tmp/guess_number.py 
+Guess what number I'm thinking of...  2
+Too low!
+walsh:~ joe$ python /tmp/guess_number.py 
+Guess what number I'm thinking of...  5
+You got it!
+"""
+
