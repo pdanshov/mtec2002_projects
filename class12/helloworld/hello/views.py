f2ac780c5e3760dd9aff47a2f6d65fc51f53c8c3
Student <student@ENT-V321-010.local>
2012-04-25 20:43:22 -0400

    4.24.12

--- class12/helloworld/hello/views.py
+++ class12/helloworld/hello/views.py
@@ -2,12 +2,13 @@
 
 from django.template import Context, loader
 from django.http import HttpResponse
+from hello import todo
 
 def index(request):
 	t = loader.get_template("hello/index.html")
 	l = ["One", "Two", "Trinity"]
 	dd = {"greeting":"hello", "gk":"gv", "three":"trinity"}
-	d = {"greeting":"hello", "gk":"gv", "three":"trinity", "my_list":l, "dict":dd}
+	d = {"greeting":"hello", "gk":"gv", "three":"trinity", "my_list":l, "dict":dd, 't':models.Todo.objects.all()}
 	c = Context(d)
 	return HttpResponse(t.render(c))
 
